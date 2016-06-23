4727a7400699a5d417bb513b321f03b856c8013e
Danny Weitekamp <dweitekamp@berkeley.edu>
2016-06-23 11:42:44 -0700

    Wrote Overfit Stopping

--- utils/callbacks.py
+++ utils/callbacks.py
@@ -41,6 +41,12 @@ class SmartCheckpoint(ModelCheckpoint):
         ModelCheckpoint.__init__(self, self.checkpointFilename,
                 monitor, verbose, save_best_only, mode)
 
+        metric_history = histDict.get(monitor, None)
+        if(metric_history != None):
+            best = metric_history[0]
+            for metric in metric_history:
+                if self.monitor_op(metric, self.best):
+                    self.best = metric
         # self.model.load_weights(self.checkpointFilename);
 
     # def on_epoch_begin(self, epoch, logs={}):
@@ -80,58 +86,65 @@ class SmartCheckpoint(ModelCheckpoint):
         self.__checkMaxEpoch(epoch + self.epochOffset)
 
 
-# class EarlyStopping(Callback):
-#     '''Stop training when a monitored quantity has stopped improving.
-#     # Arguments
-#         monitor: quantity to be monitored.
-#         patience: number of epochs with no improvement
-#             after which training will be stopped.
-#         verbose: verbosity mode.
-#         mode: one of {auto, min, max}. In 'min' mode,
-#             training will stop when the quantity
-#             monitored has stopped decreasing; in 'max'
-#             mode it will stop when the quantity
-#             monitored has stopped increasing.
-#     '''
-#     def __init__(self, monitor='val_loss', patience=0, verbose=0, mode='auto'):
-#         super(EarlyStopping, self).__init__()
+class OverfitStopping(EarlyStopping):
+    '''Stop training when a monitored quantity has stopped improving.
+    # Arguments
+        monitor: quantity to be monitored.
+        patience: number of epochs with no improvement
+            after which training will be stopped.
+        verbose: verbosity mode.
+        mode: one of {auto, min, max}. In 'min' mode,
+            training will stop when the quantity
+            monitored has stopped decreasing; in 'max'
+            mode it will stop when the quantity
+            monitored has stopped increasing.
+    '''
+    def __init__(self, monitor='val_loss', comparison_monitor="loss", max_percent_diff=.1, patience=0, verbose=0, mode='auto'):
+        self.comparison_monitor = comparison_monitor
+        self.max_percent_diff = max_percent_diff
+        EarlyStopping.__init__(monitor, patience=patience, verbose=verbose, mode=mode)
+        # self.monitor = monitor
+        # self.patience = patience
+        # self.verbose = verbose
+        # self.wait = 0
 
-#         self.monitor = monitor
-#         self.patience = patience
-#         self.verbose = verbose
-#         self.wait = 0
+        # if mode not in ['auto', 'min', 'max']:
+        #     warnings.warn('EarlyStopping mode %s is unknown, '
+        #                   'fallback to auto mode.' % (self.mode), RuntimeWarning)
+        #     mode = 'auto'
 
-#         if mode not in ['auto', 'min', 'max']:
-#             warnings.warn('EarlyStopping mode %s is unknown, '
-#                           'fallback to auto mode.' % (self.mode), RuntimeWarning)
-#             mode = 'auto'
+        # if mode == 'min':
+        #     self.monitor_op = np.less
+        # elif mode == 'max':
+        #     self.monitor_op = np.greater
+        # else:
+        #     if 'acc' in self.monitor:
+        #         self.monitor_op = np.greater
+        #     else:
+        #         self.monitor_op = np.less
 
-#         if mode == 'min':
-#             self.monitor_op = np.less
-#         elif mode == 'max':
-#             self.monitor_op = np.greater
-#         else:
-#             if 'acc' in self.monitor:
-#                 self.monitor_op = np.greater
-#             else:
-#                 self.monitor_op = np.less
+    def on_train_begin(self, logs={}):
+        self.wait = 0       # Allow instances to be re-used
+       # self.best = np.Inf if self.monitor_op == np.less else -np.Inf
 
-#     def on_train_begin(self, logs={}):
-#         self.wait = 0       # Allow instances to be re-used
-#         self.best = np.Inf if self.monitor_op == np.less else -np.Inf
+    def on_epoch_end(self, epoch, logs={}):
+        current = logs.get(self.monitor)
+        current_comp = logs.get(self.comparison_monitor)
+        signed_percent_difference = (current - current_comp)/((current + current_comp)/2)
 
-#     def on_epoch_end(self, epoch, logs={}):
-#         current = logs.get(self.monitor)
-#         if current is None:
-#             warnings.warn('Early stopping requires %s available!' %
-#                           (self.monitor), RuntimeWarning)
+        if current is None:
+            warnings.warn('Overfit stopping requires %s available!' %
+                          (self.monitor), RuntimeWarning)
+        if current_comp is None:
+            warnings.warn('Overfit stopping requires %s available!' %
+                          (self.comparison_monitor), RuntimeWarning)
 
-#         if self.monitor_op(current, self.best):
-#             self.best = current
-#             self.wait = 0
-#         else:
-#             if self.wait >= self.patience:
-#                 if self.verbose > 0:
-#                     print('Epoch %05d: early stopping' % (epoch))
-#                 self.model.stop_training = True
-#             self.wait += 1
+        if self.monitor_op(signed_percent_difference, self.max_percent_diff):
+            self.best = current
+            self.wait = 0
+        else:
+            if self.wait >= self.patience:
+                if self.verbose > 0:
+                    print('Epoch %05d: overfit stopping' % (epoch))
+                self.model.stop_training = True
+            self.wait += 1
