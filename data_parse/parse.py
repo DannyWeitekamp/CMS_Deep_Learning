1cc237a7c008c779b94a7b31756b810623de9673
Danny Weitekamp <dweitekamp@berkeley.edu>
2016-06-22 20:29:33 -0700

    Added data parse util

--- data_parse/parse.py
+++ data_parse/parse.py
@@ -1,22 +1,12 @@
-#import ROOT in batch mode
-import sys
-oldargv = sys.argv[:]
-sys.argv = [ '-b-' ]
-import ROOT
-ROOT.gROOT.SetBatch(True)
-sys.argv = oldargv
+import sys, os
+if __package__ is None:
+	import sys, os
+	sys.path.append(os.path.realpath("../../"))
+from CMS_SURF_2016.utils.data_parse import extract_ROOT_data_to_hdf
 
-# load FWLite C++ libraries
-ROOT.gSystem.Load("libFWCoreFWLite.so");
-ROOT.gSystem.Load("libDataFormatsFWLite.so");
-ROOT.FWLiteEnabler.enable()
 
-# load FWlite python libraries
-from DataFormats.FWLite import Handle, Events
-
-# open file (you can use 'edmFileUtil -d /store/whatever.root' to get the physical file name)
-events = Events("../data/ttbar_13TeV_80.root")
-for i,event in enumerate(events):
-    print "\nEvent", i
-    if i > 10: break
-
+extract_ROOT_data_to_hdf("../data/ttbar_13TeV_80.root",
+						 "ttbar_13TeV_80.h5",
+						  ["Jet.T","Jet.Eta", "Jet.Phi", "Jet.PT", "Jet.Flavor", "Jet.Charge"],
+						  columns=["T", "Eta", "Phi", "PT", "Flavor", "Charge"],
+						  verbosity=1)
---
+++ utils/data_parse.py
@@ -0,0 +1,81 @@
+''' 
+data_parse.py
+Contains tools for parsing High Energy Physics data
+Author: Danny Weitekamp
+e-mail: dannyweitekamp@gmail.com
+''' 
+import ROOT
+from ROOT import TTree
+import pandas as pd
+
+ 
+
+
+def extract_ROOT_data_to_hdf(inputfilepath, outputfilepath , leaves,
+							trees=None,
+							columns=None,
+							entrylabel="Event",
+							verbosity=1):
+	'''Extracts values from a .root file and writes them to pandas frame stored as an hdf5 file.
+		 Essentially takes root data out of its tree format and puts it in a table.
+    # Arguments
+        inputfilepath: The path to the .root file held locally or in a distributed system
+        outputfilepath: The path to the HDF5 file
+        leaves: A list of the names of the leaves that need to be extracted 
+        trees: A list of the trees in the .root file that need to be parsed.
+        	Reads all trees in the file if not set.
+        columns: A list of the column names for the table. Uses the leaf names
+        	if not seet
+        verbosity: 0:No print output, 1:Some, 2:A lot of the table is printed
+    '''
+	f = ROOT.TFile.Open(inputfilepath)
+
+	if(verbosity > 0): print("Extracting data from " + inputfilepath)
+
+	if(trees == None):
+		trees = [x.GetName() for x in f.GetListOfKeys() if isinstance(x.ReadObj(), TTree)]
+	if(verbosity > 0): print("Using trees: " + ', '.join(trees))
+	if(verbosity > 0): print("Extracting Leaves: " + ', '.join(leaves))
+	
+	if(columns == None):
+		columns = leaves
+	else:
+		assert len(leaves) == len(columns), "columns input length mismatch: \
+			len(leaves) = %r , len(columns) = %r" % (len(leaves), len(columns))
+		if(verbosity > 0): print("Renaming to: " + ', '.join(columns))
+
+	dataDict = {}
+	dataDict[entrylabel] = []
+	for name in columns:
+		dataDict[name] = []
+
+	for tree_name in trees:
+		tree = f.Get(tree_name)
+		l_leaves = []
+		for leaf in leaves:
+			l_leaves.append(tree.GetLeaf(leaf))
+
+		n_entries=tree.GetEntries()
+		for entry in range(n_entries):
+			#
+			entry_num = tree.GetEntry(entry)
+			nValues = l_leaves[0].GetLen()
+			dataDict[entrylabel] = dataDict[entrylabel] + [entry_num]*nValues
+			for j, l_leaf in enumerate(l_leaves):
+				assert l_leaf.GetLen() == nValues, "%r entries in leaf '%r' does not match  \
+					%r entries in leaf '%r'" % (l_leaf.GetLen(),l_leaf.GetName(), nValues, l_leaves[0].GetName())
+				for i in range(nValues):
+					dataDict[columns[j]].append(l_leaf.GetValue(i))
+
+
+	dataframe = pd.DataFrame(dataDict, columns=[entrylabel]+columns)
+	# if(verbosity > 0):
+	if(verbosity > 1):
+		with pd.option_context('display.max_rows', 999, 'display.max_columns', 10): 
+			print(dataframe)
+	elif(verbosity > 0):
+		with pd.option_context('display.max_rows', 10 , 'display.max_columns', 10): 
+			print(dataframe)
+
+	if(verbosity > 0): print("Exporting To " + outputfilepath)
+	dataframe.to_hdf(outputfilepath,'data')
