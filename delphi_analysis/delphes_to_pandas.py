0b51240d0cd13937589d2c7a6c49176509c352c0
Danny Weitekamp <dweitekamp@berkeley.edu>
2016-07-07 17:17:21 -0700

    fixed indentation

--- delphi_analysis/delphes_to_pandas.py
+++ delphi_analysis/delphes_to_pandas.py
@@ -20,65 +20,65 @@ import getopt
 
 
 def cullNonObservables(frame):
-    #Status of 1 means that the particle is a stable product
-    stable_cond = frame["Status"] == 1 
-    #All even leptons are neutrinos which we can't measure
-    notNeutrino_cond = frame["PID"] % 2 == 1
-    parton_cond = np.abs(frame["PID"]) <= 8
-    #Get all entries that satisfy the conditions
-    frame = frame[stable_cond & notNeutrino_cond]
-    #Drop the Status frame since we only needed it to see if the particle was stable
-    frame = frame.drop(["Status"], axis=1)
-    return frame
+   #Status of 1 means that the particle is a stable product
+   stable_cond = frame["Status"] == 1 
+   #All even leptons are neutrinos which we can't measure
+   notNeutrino_cond = frame["PID"] % 2 == 1
+   parton_cond = np.abs(frame["PID"]) <= 8
+   #Get all entries that satisfy the conditions
+   frame = frame[stable_cond & notNeutrino_cond]
+   #Drop the Status frame since we only needed it to see if the particle was stable
+   frame = frame.drop(["Status"], axis=1)
+   return frame
 
 
 
 #http://stackoverflow.com/questions/3678869/pythonic-way-to-combine-two-lists-in-an-alternating-fashion
 def roundrobin(*iterables):
-    "roundrobin('ABC', 'D', 'EF') --> A D E B F C"
-    # Recipe credited to George Sakkis
-    pending = len(iterables)
-    nexts = cycle(iter(it).next for it in iterables)
-    while pending:
-        try:
-            for next in nexts:
-                yield next()
-        except StopIteration:
-            pending -= 1
-            nexts = cycle(islice(nexts, pending))
+   "roundrobin('ABC', 'D', 'EF') --> A D E B F C"
+   # Recipe credited to George Sakkis
+   pending = len(iterables)
+   nexts = cycle(iter(it).next for it in iterables)
+   while pending:
+      try:
+         for next in nexts:
+             yield next()
+      except StopIteration:
+         pending -= 1
+         nexts = cycle(islice(nexts, pending))
 
 
 def storeAllUnjoined(filepath, outputdir, rerun=False):
-    lst = [         ("NumValues", "getPandasNumValues(filepath)"),
-                    ("Photon", "getPandasPhotons(filepath)"),
-                    ("Electron", "getPandasElectrons(filepath)"),
-                    ("MuonTight", "getPandasTightMuons(filepath)"),
-                    ("MissingET", "getPandasMissingET(filepath)"),
-                    ("EFlowPhoton", "getPandasEFlowParticle(filepath, name='EFlowNeutralHadron')"),
-                    ("EFlowNeutralHadron", "getPandasEFlowParticle(filepath, name='EFlowPhoton')"),
-                    ("EFlowTrack", "getPandasEFlowTrack(filepath)")]
+   lst = [        ("NumValues", "getPandasNumValues(filepath)"),
+                  ("Photon", "getPandasPhotons(filepath)"),
+                  ("Electron", "getPandasElectrons(filepath)"),
+                  ("MuonTight", "getPandasTightMuons(filepath)"),
+                  ("MissingET", "getPandasMissingET(filepath)"),
+                  ("EFlowPhoton", "getPandasEFlowParticle(filepath, name='EFlowNeutralHadron')"),
+                  ("EFlowNeutralHadron", "getPandasEFlowParticle(filepath, name='EFlowPhoton')"),
+                  ("EFlowTrack", "getPandasEFlowTrack(filepath)")]
     
-    filename = os.path.splitext(ntpath.basename(filepath))[0]
-    out_file = outputdir + filename + ".h5"
-    print(out_file)
-    store = pd.HDFStore(out_file)
-    keys = store.keys()
-    print("KEYS:", keys)
-    for tup in lst:
-        if(rerun or (("/"+tup[0]) in keys) == False):
-            #print(rerun, ~os.path.isfile(out_file), out_file)
-            frame = eval(tup[1])
-            print(frame)
-            store.put(tup[0], frame, format='table')
+   filename = os.path.splitext(ntpath.basename(filepath))[0]
+   out_file = outputdir + filename + ".h5"
+   print(out_file)
+   store = pd.HDFStore(out_file)
+   keys = store.keys()
+   print("KEYS:", keys)
+   for tup in lst:
+      if(rerun or (("/"+tup[0]) in keys) == False):
+         #print(rerun, ~os.path.isfile(out_file), out_file)
+         frame = eval(tup[1])
+         print(frame)
+         store.put(tup[0], frame, format='table')
    store.close()
             
         
 
 def storeAllJoined(filepath, outputfile, rerun=False):
-    if(rerun or os.path.isfile(outputfile) == False):
-        #print(rerun, ~os.path.isfile(out_file), out_file)
-        frame = getPandasAll(filepath)
-        frame.to_hdf(outputfile, 'data', mode='w', data_columns=True)
+   if(rerun or os.path.isfile(outputfile) == False):
+      #print(rerun, ~os.path.isfile(out_file), out_file)
+      frame = getPandasAll(filepath)
+      frame.to_hdf(outputfile, 'data', mode='w', data_columns=True)
     
 
     
@@ -86,34 +86,34 @@ def makeJobs(filename, job_types,
              directory="/data/shared/Delphes/",
              unjoined_folder="/pandas_unjoined/",
              joined_folder="/pandas_joined/"):
-    files = glob.glob(directory + filename + "/*.root")
-    unjoined_dir = directory + filename + unjoined_folder
-    joined_dir = directory + filename + joined_folder
-    if not os.path.exists(joined_dir):
-        os.makedirs(joined_dir)
-    if not os.path.exists(unjoined_dir):
-        os.makedirs(unjoined_dir)
+   files = glob.glob(directory + filename + "/*.root")
+   unjoined_dir = directory + filename + unjoined_folder
+   joined_dir = directory + filename + joined_folder
+   if not os.path.exists(joined_dir):
+      os.makedirs(joined_dir)
+   if not os.path.exists(unjoined_dir):
+      os.makedirs(unjoined_dir)
     
-    jobs = []
+   jobs = []
     
-    for f in files:
-        f_name = os.path.splitext(ntpath.basename(f))[0]
-        for j_type in job_types:
-            if(j_type == "unjoined"):
-                jobs.append((j_type,f, unjoined_dir))
-            elif(j_type == "joined"):
-                jobs.append((j_type,f, joined_dir + f_name + ".h5"))
+   for f in files:
+      f_name = os.path.splitext(ntpath.basename(f))[0]
+      for j_type in job_types:
+         if(j_type == "unjoined"):
+            jobs.append((j_type,f, unjoined_dir))
+         elif(j_type == "joined"):
+            jobs.append((j_type,f, joined_dir + f_name + ".h5"))
 
     return jobs
 
 def doJob(job):
-    if(job[0] == "unjoined"):
-        #print("Started: ", job[1])
-        storeAllUnjoined(job[1], job[2])
-    elif(job[0] == "joined"):
-        #print(job)
-        storeAllJoined(job[1], job[2])
-    return job[1]
+   if(job[0] == "unjoined"):
+      #print("Started: ", job[1])
+      storeAllUnjoined(job[1], job[2])
+   elif(job[0] == "joined"):
+      #print(job)
+      storeAllJoined(job[1], job[2])
+   return job[1]
 
 
 def main(data_dir, argv):
