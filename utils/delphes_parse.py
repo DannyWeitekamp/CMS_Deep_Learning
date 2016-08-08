8c878dc4efc5c45107e533e61cdb2b5b29226031
Danny Weitekamp <dweitekamp@berkeley.edu>
2016-08-08 16:14:45 -0700

    testing new meta data reads for msgpack

--- utils/delphes_parse.py
+++ utils/delphes_parse.py
@@ -394,17 +394,21 @@ def doJob(job, redo=False):
     return f
 
 def msgpack_assertMeta(filename, frames=None, redo=False):
-    if(frames == None):
-        print("Bulk reading .msg for metaData assertion. Be patient, reading in slices not supported.")
-        print(filename)
-        frames = pd.read_msgpack(filename)
     meta_out_file = filename.replace(".msg", ".meta")
     print(meta_out_file)
-    if(not os.path.exists(meta_out_file) or redo):
-        meta_frames = {"NumValues" : frames["NumValues"]}
-        pd.to_msgpack(meta_out_file, meta_frames)
-    else:
+    meta_frames = None
+    if(os.path.exists(meta_out_file) and not redo):
         meta_frames = pd.read_msgpack(meta_out_file)
+    if(meta_frames == None):
+        if(frames == None):
+            print("Bulk reading .msg for metaData assertion. Be patient, reading in slices not supported.")
+            print(filename)
+            frames = pd.read_msgpack(filename)
+        meta_frames = {"NumValues" : frames["NumValues"]}
+           
+    if(not os.path.exists(meta_out_file) or redo):
+        pd.to_msgpack(meta_out_file, meta_frames)
+
     return meta_frames
 
 
