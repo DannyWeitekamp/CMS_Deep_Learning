483fcd76159fc692c156733ea672e916cfccda09
Danny Weitekamp <dweitekamp@berkeley.edu>
2016-08-12 14:28:20 -0700

    Plots and metrics now separate

--- utils/metrics.py
+++ utils/metrics.py
@@ -1,70 +1,111 @@
-'''
-metrics.py
-Contains custom metric utilities for ploting and displaying metrics.
-Author: Danny Weitekamp
-e-mail: dannyweitekamp@gmail.com
-''' 
+import types
+from keras.metrics import *
+def accVsEventChar(model,
+                   data,
+                   char,
+                   observ,
+                   objects,
+                   num_samples=None,
+                   char2=None,
+                   bins=20,
+                   possible_observables=['E/c', 'Px', 'Py', 'Pz', 'Charge', "PT_ET", "Eta", "Phi", "Dxy_Ehad_Eem"],
+                   possible_objects=["Electron", "MuonTight", "Photon", "MissingET","EFlowPhoton", "EFlowNeutralHadron", "EFlowTrack"],
+                   equalBins=False):
+    predictions = None
+    characteristics = None
+    if(isinstance(data, DataProcedure)):
+        data = data.getData()
+    if(isinstance(data, tuple)):
+        data = [data]
+    elif(isinstance(data, types.GeneratorType)):
+        if(num_samples == None): raise ValueError("Must provide argument num_samples if argument 'data' is a generator")
+    else:
+        raise TypeError("Arguement 'data' is not DataProcedure, Generator or (X,Y) instead got,%r" % type(data))
+    if(num_samples == None):
+        num_samples = 0
+        for out in data:
+            if(isinstance(out,DataProcedure)):
+                X,Y = out.getData()
+            else:
+                X,Y = out
+            #if(not isinstance(X, list)): Y = [Y]
+            if(not isinstance(Y, list)): Y = [Y]
+            num_samples += Y[0].shape[0]
+    predictions = [None] * num_samples
+    characteristics = [None] * num_samples
+    y_vals = [None] * num_samples
+    print(possible_objects)
+    print(objects)
+    if(not isinstance(objects, list)): objects = [objects]
+    objects = [o if isinstance(o, int) else possible_objects.index(o) for o in objects]
+    observ = observ if isinstance(observ, int) else possible_observables.index(observ)
+    if(char2 == None): char2 = char
+    num_read = 0
+    global_batch_size = None
+    for i, out in enumerate(data):
+        if(isinstance(out,DataProcedure)):
+            X,Y = out.getData()
+        else:
+            X,Y = out
+        batch_size = Y[0].shape[0]
+        if(global_batch_size == None): global_batch_size = batch_size
+        if(not isinstance(X, list)): X = [X]
+        if(not isinstance(Y, list)): Y = [Y]
+        batch_predicts = model.predict_on_batch(X)
 
-import matplotlib.pyplot as plt
-import numpy as np
-from keras.callbacks import History
-def plot_history( histories, plotLoss=True, plotAccuracy=True):
-    """ Plots an array of training histories against each other
-        -input: [(String label, History hist), .... ]
-        -Adopted from Jean-Roch Vlimant's Kreas tutorial"""
-
-    colors=[tuple(np.random.random(3)) for i in range(len(histories))]
-    if(plotLoss):
-        plt.figure(figsize=(10,10))
-        plt.xlabel('Epoch')
-        plt.ylabel('loss')
-        plt.title('Training Error by Epoch')
-        for label,history in histories:
-            if(isinstance(history, History)):
-                history = history.history
-            color = colors[i]
-            l = label
-            vl= label+" validation"
-            if 'acc' in history:
-                l+=' (acc %2.4f)'% (history['acc'][-1])
-                do_acc = True
-            if 'val_acc' in history:
-                vl+=' (acc %2.4f)'% (history['val_acc'][-1])
-                do_acc = True
-            plt.plot(history['loss'], label=l, color=color)
-            if 'val_loss' in history:
-                plt.plot(history['val_loss'], lw=2, ls='dashed', label=vl, color=color)
-        plt.legend()
-        plt.yscale('log')
-        plt.show()
-
-    if(plotAccuracy):
-        plt.figure(figsize=(10,10))
-        plt.xlabel('Epoch')
-        plt.ylabel('Accuracy')
-        for i,(label,history) in enumerate(histories):
-            if(isinstance(history, History)):
-                history = history.history
-            color = colors[i]
-            if 'acc' in history:
-                plt.plot(history['acc'], lw=2, label=label+" accuracy", color=color)
-            if 'val_acc' in history:
-                plt.plot(history['val_acc'], lw=2, ls='dashed', label=label+" validation accuracy", color=color)
-        plt.legend(loc='lower right')
-        plt.show()
-
-
-def print_accuracy( p, test_target):
-    """ Prints the accuracy of a prediction array.
-        -Taken from Jean-Roch Vlimant's Kreas tutorial"""
-    p_cat = np.argmax(p,axis=1)
-    print "Fraction of good prediction"
-    print len(np.where( p_cat == test_target)[0])
-    print len(np.where( p_cat == test_target )[0])/float(len(p_cat)),"%"
+        obj_chars = np.array([char(X[o][:,:,observ], axis=1) for o in objects])
+        assert obj_chars.shape[0] == len(obj_chars)
+        assert obj_chars.shape[1] == batch_size
+        batch_chars = char2(obj_chars, axis=0)
+        for j in range(batch_size):
+            #print(type(c), type(p))
+            #print(c, p.shape)
+            #print(type(c),type(p),type(y))
+            index = i*global_batch_size+j
+            #print(index)
+            characteristics[index] = batch_chars[j]
+            predictions[index] = batch_predicts[j]
+            y_vals[index] = Y[0][j]
+        num_read += batch_size
+        if(num_read >= num_samples):
+            print(num_read,num_samples, len(characteristics), characteristics[:10])
+            break
+    characteristics = np.array(characteristics)
+    predictions = np.array(predictions)
+    y_vals = np.array(y_vals)
     
-def print_accuracy_m( model, test_data, test_target):
-    """ Prints the accuracy of a compiled model."""
-    ##figure out the shape of the input expected
-    #if hasattr('input_dim', model.layers[0]):
-    p=model.predict(test_data)
-    print_accuracy(p, test_target)
+    sorted_indicies = np.argsort(characteristics)
+    
+    characteristics = characteristics[sorted_indicies]
+    predictions = predictions[sorted_indicies]
+    y_vals = y_vals[sorted_indicies]
+    
+    min_char = characteristics[0]
+    max_char = characteristics[characteristics.shape[0]-1]
+    if(not equalBins):
+        stride = (max_char-min_char)/bins
+        split_vals = [min_char+stride*(i+1) for i in range(bins-1)]
+        split_at = characteristics.searchsorted(split_vals)
+    else:
+        stride = characteristics.shape[0]/float(bins)
+        split_at = [int(stride*float(i+1)) for i in range(bins-1)]
+       
+    predict_bins = np.split(predictions, split_at)
+    y_bins = np.split(y_vals, split_at)
+    true_false_bins = [np.equal(np.argmax(p, axis=-1),np.argmax(y, axis=-1)).astype("float64") for (p,y) in zip(predict_bins, y_bins)]
+    
+    out_bins = []
+    prevmax = min_char
+    for i,tf in enumerate(true_false_bins):
+        b = {}
+        num = tf.shape[0]
+        b["y"] = np.mean(tf)
+        b["error"] = np.std(tf)/np.sqrt(num)
+        b["num_samples"] = num
+        b["min_bin_x"] = prevmax
+        if(i == len(true_false_bins)-1):
+            b["max_bin_x"] = max_char
+        else:
+            b["max_bin_x"] = prevmax = characteristics[split_at[i]]
+        out_bins.append(b)
+    return out_bins
---
+++ utils/plot.py
@@ -0,0 +1,89 @@
+'''
+metrics.py
+Contains custom utilities for ploting and displaying training data.
+Author: Danny Weitekamp
+e-mail: dannyweitekamp@gmail.com
+''' 
+
+import matplotlib.pyplot as plt
+import numpy as np
+from keras.callbacks import History
+
+def plot_history( histories, plotLoss=True, plotAccuracy=True):
+    """ Plots an array of training histories against each other
+        -input: [(String label, History hist), .... ]
+        -Adopted from Jean-Roch Vlimant's Kreas tutorial"""
+
+    colors=[tuple(np.random.random(3)) for i in range(len(histories))]
+    if(plotLoss):
+        plt.figure(figsize=(10,10))
+        plt.xlabel('Epoch')
+        plt.ylabel('loss')
+        plt.title('Training Error by Epoch')
+        for label,history in histories:
+            if(isinstance(history, History)):
+                history = history.history
+            color = colors[i]
+            l = label
+            vl= label+" validation"
+            if 'acc' in history:
+                l+=' (acc %2.4f)'% (history['acc'][-1])
+                do_acc = True
+            if 'val_acc' in history:
+                vl+=' (acc %2.4f)'% (history['val_acc'][-1])
+                do_acc = True
+            plt.plot(history['loss'], label=l, color=color)
+            if 'val_loss' in history:
+                plt.plot(history['val_loss'], lw=2, ls='dashed', label=vl, color=color)
+        plt.legend()
+        plt.yscale('log')
+        plt.show()
+
+    if(plotAccuracy):
+        plt.figure(figsize=(10,10))
+        plt.xlabel('Epoch')
+        plt.ylabel('Accuracy')
+        for i,(label,history) in enumerate(histories):
+            if(isinstance(history, History)):
+                history = history.history
+            color = colors[i]
+            if 'acc' in history:
+                plt.plot(history['acc'], lw=2, label=label+" accuracy", color=color)
+            if 'val_acc' in history:
+                plt.plot(history['val_acc'], lw=2, ls='dashed', label=label+" validation accuracy", color=color)
+        plt.legend(loc='lower right')
+        plt.show()
+
+
+def print_accuracy( p, test_target):
+    """ Prints the accuracy of a prediction array.
+        -Taken from Jean-Roch Vlimant's Kreas tutorial"""
+    p_cat = np.argmax(p,axis=1)
+    print "Fraction of good prediction"
+    print len(np.where( p_cat == test_target)[0])
+    print len(np.where( p_cat == test_target )[0])/float(len(p_cat)),"%"
+    
+def print_accuracy_m( model, test_data, test_target):
+    """ Prints the accuracy of a compiled model."""
+    ##figure out the shape of the input expected
+    #if hasattr('input_dim', model.layers[0]):
+    p=model.predict(test_data)
+    print_accuracy(p, test_target)
+
+
+
+
+def plotBins(bins, min_samples=10, title='', xlabel='', ylabel='', color='g'):
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    for b in bins:
+        
+        if(b["num_samples"] >= min_samples):
+            width = b["max_bin_x"]-b["min_bin_x"]
+            x = b["min_bin_x"]
+            ax.bar(x, b["y"], width=width, yerr=b["error"], color=color, ecolor='k', alpha=.8)
+    ax.set_title(title, size=16)
+    ax.set_xlabel(xlabel, size=14)
+    ax.set_ylabel(ylabel, size=14)
+   
+    plt.show()
