905dfe256e1162e576e25922d573be9beef1d22d
Danny Weitekamp <dannyweitekamp@gmail.com>
2016-08-15 14:03:05 -0700

    Added scripts to CMS_SURF_2016

---
+++ scripts/runTrial.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+module load Python/2.7.11-CrayGNU-2016.03
+module load pycuda/2015.1.3-CrayGNU-2016.03-Python-2.7.11
+
+module unload gcc/4.9.3
+module load gcc/4.8.2
+source /scratch/daint/vlimant/p2.7/bin/activate
+
+python /scratch/daint/dweiteka/scripts/runTrial.py $1 $2 $3 $4
+
