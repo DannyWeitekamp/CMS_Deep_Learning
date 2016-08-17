768c9bd4a88dfd0f17b2a8996ba0a48c71f942f4
Danny Weitekamp <dannyweitekamp@gmail.com>
2016-08-17 00:48:19 -0700

    fix error in gitutils

--- utils/gitutils.py
+++ utils/gitutils.py
@@ -2,11 +2,11 @@ from  CMS_SURF_2016.utils.archiving import KerasTrial
 import os, sys
 
 def addCommitPushDir(folder, remote="origin", branch="master"):
-	out1 = os.popen("git --git-dir=%s.git --work-tree=%s add ." % (folder,folder)).read()
+	out1 = os.popen("cd %s && git add ." % (folder)).read()
 	print(out1)
-	out2 = os.popen("git --git-dir=%s.git --work-tree=%s commit -a -m 'added %r'" % (folder,folder, folder)).read()
+	out2 = os.popen("cd %s && git commit -a -m 'added %r'" % (folder, folder)).read()
 	print(out2)
-	out3 = os.popen("git --git-dir=%s.git --work-tree=%s push %r %r" % (folder,folder, remote, branch)).read()
+	out3 = os.popen("cd %s && git push %r %r" % (folder, remote, branch)).read()
 	print(out3)
 
 def commitAllTrials(archive_dir,remote="origin", branch="master"):
