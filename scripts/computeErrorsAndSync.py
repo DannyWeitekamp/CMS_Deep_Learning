cba1b6c5f06076182a7b987c2d4a32dd90657c53
Danny Weitekamp <dannyweitekamp@gmail.com>
2016-09-15 19:13:06 -0700

    Added computeErrorsANdSync

--- scripts/computeErrorsAndSync.py
+++ scripts/computeErrorsAndSync.py
@@ -5,17 +5,17 @@ repo_outerdir = "/scratch/daint/dweiteka/"
 if(not repo_outerdir in sys.path):
     sys.path.append(repo_outerdir)
 #%matplotlib inline
-import sys, os
-if __name__ == "__main__":
-    username = "dweiteka"
-    if(len(sys.argv) > 1):
-        username = sys.argv[1]
+# import sys, os
+# if __name__ == "__main__":
+#     username = "dweiteka"
+#     if(len(sys.argv) > 1):
+#         username = sys.argv[1]
 
 
 import socket
 if("daint" in socket.gethostname()):
-    DELPHES_DIR = "/scratch/daint/" + username +  "/Delphes/"
-    SOFTWAR_DIR = "/scratch/daint/" + username + "/"
+    DELPHES_DIR = "/scratch/daint/" + "dweiteka" +  "/Delphes/"
+    SOFTWAR_DIR = "/scratch/daint/" + "dweiteka" + "/"
     STORE_TYPE = "msg"
 else:
     DELPHES_DIR = "/data/shared/Delphes/"
@@ -45,15 +45,15 @@ while(time.clock() - start_time < 5):
 if(not imports_ok):
     raise IOError("Failed to import CMS_SURF_2016 or keras, ~/.keras/keras.json is probably being read by multiple processes")
 
+print("WE Got Imports")
+#def main():
+trials = get_all_trials(archive_dir)
+print("Got %r Trials" % len(trials))
+for trial in trials:
+    print("START %r" % trial.hash())
+    print(getTrialError(trial,custom_objects={"Lorentz":Lorentz,"Slice": Slice}, ignoreAssert=True))
+    #rsyncStorable(trial.hash(), archive_dir, "dweitekamp@titans.hep.caltech.edu:/data/shared/Delphes/CSCS_output/keras_archive")
 
-def main():
-    trials = get_all_trials(archive_dir)
-    print("Got %r Trials" % len(trials))
-    for trial in trials:
-        print("START %r" % trial.hash())
-        print(getTrialError(trial,custom_objects={"Lorentz":Lorentz,"Slice": Slice}, ignoreAssert=True))
-        #rsyncStorable(trial.hash(), archive_dir, "dweitekamp@titans.hep.caltech.edu:/data/shared/Delphes/CSCS_output/keras_archive")
-
-if __name__ == "__main__":
-    main()
+# if __name__ == "__main__":
+    # main()
 
