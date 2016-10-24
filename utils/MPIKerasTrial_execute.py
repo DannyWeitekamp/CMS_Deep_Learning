94380071b03281d37304cd908c8c13cacc032516
Danny Weitekamp <dweitekamp@titans.hep.caltech.edu>
2016-10-23 20:55:31 -0700

    tabs to spaces

--- utils/MPIKerasTrial_execute.py
+++ utils/MPIKerasTrial_execute.py
@@ -5,14 +5,14 @@ from CMS_SURF_2016.utils.MPIArchiving import MPI_KerasTrial
 
 print("Mooop")
 if(len(sys.argv) != 3):
-	raise ValueError("MPIKerasTrail_execute.py -- Incorrect number of arguments.")
+    raise ValueError("MPIKerasTrail_execute.py -- Incorrect number of arguments.")
 archive_dir = sys.argv[1]
 hashcode = sys.argv[2]
 trial = KerasTrial.find_by_hashcode(archive_dir, hashcode)
 if(trial == None):
-	raise ValueError("hashcode does not exist")
+    raise ValueError("hashcode does not exist")
 if(not isinstance(trial, MPI_KerasTrial)):
-	raise TypeError("Trial is not MPI_KerasTrial, got type %r" % type(trial))
+    raise TypeError("Trial is not MPI_KerasTrial, got type %r" % type(trial))
 trial.execute(isMPI_Instance=True)
 print(sys.argv[0])
 print(sys.argv[1])
