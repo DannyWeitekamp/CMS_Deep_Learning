f182f463029cc1182f3cb9a17f3e7ed0fc0c1b8f
Danny Weitekamp <dannyweitekamp@gmail.com>
2016-11-28 22:46:36 -0800

    Assert NumVal is int

--- tests/parse_test.py
+++ tests/parse_test.py
@@ -3,5 +3,5 @@ if __package__ is None:
     sys.path.append(os.path.realpath("../"))
 from utils.delphes_parse import delphes_to_pandas
 
-df = delphes_to_pandas("../delphi_analysis/ttbar_13TeV_106.h5")
+df = delphes_to_pandas("../delphi_analysis/ttbar_13TeV_106.h5", fixedNum=100)
 print(df)
