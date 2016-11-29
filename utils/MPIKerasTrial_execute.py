0f8ecd2f3714e4d33c04e0318fdfe6b312ff258d
Danny Weitekamp <dannyweitekamp@gmail.com>
2016-11-28 20:06:03 -0800

    Everything class variables

--- utils/MPIKerasTrial_execute.py
+++ utils/MPIKerasTrial_execute.py
@@ -15,7 +15,7 @@ parser = argparse.ArgumentParser()
 # parser.add_argument('--foo', help='foo help')
 parser.add_argument('archive_dir',help='archive directory of the trial')
 parser.add_argument('hashcode',help='hashcode for the trial')
-parser.add_argument('--masters',help='number of masters', , default=1)
+parser.add_argument('--masters',help='number of masters', default=1)
 parser.add_argument('--max-gpus',help='maximum number of gpus to use', dest='max_gpus', default=-1)
 
 args = parser.parse_args()
