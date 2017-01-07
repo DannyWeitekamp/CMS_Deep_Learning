84eb2c4747040507ba4c2128030b5f5f5ce45708
Danny Weitekamp <dweitekamp@titans.hep.caltech.edu>
2017-01-07 15:25:25 -0800

    test parser now w/o errors

--- tests/parse_test.py
+++ tests/parse_test.py
@@ -1,25 +1,24 @@
 import os
 import sys
 import unittest
-import CMS_Deep_Learning
 
 if __package__ is None:
     sys.path.append(os.path.realpath("../"))
     sys.path.append(os.path.realpath("../../"))
+import CMS_Deep_Learning
 from CMS_Deep_Learning.preprocessing.delphes_parse import delphes_to_pandas
-print(os.path.realpath("/data/shared/Delphes/wjets_lepFilter_13TeV/wjets_lepFilter_13TeV_183.root"))
-df = delphes_to_pandas(os.path.realpath("/data/shared/Delphes/wjets_lepFilter_13TeV/wjets_lepFilter_13TeV_183.root"),fixedNum=100)
-
-
 
 class TestDelphesParser(unittest.TestCase):
-    def sanityCheck(self):
+    def test_sanity(self):
         p = os.path.dirname(os.path.abspath(CMS_Deep_Learning.__file__))
-        loc = p + "../data/qcd_lepFilter_13TeV_2.root"
+        loc = p + "/../data/qcd_lepFilter_13TeV_2.root"
         loc = os.path.abspath(loc)
+        print(loc)
         self.assertTrue(os.path.exists(loc))
-        df = delphes_to_pandas(loc, fixedNum=100)
+        df = delphes_to_pandas(loc, fixedNum=10)
+        print(df)
 
-
+if __name__ == '__main__':
+    unittest.main()
 
 print(df)
