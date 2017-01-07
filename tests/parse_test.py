e37995ba144b87cd7295ef764bf90bb1a8a6f3cf
Danny Weitekamp <dweitekamp@titans.hep.caltech.edu>
2017-01-06 22:25:38 -0800

    added small root files for test

--- tests/parse_test.py
+++ tests/parse_test.py
@@ -4,7 +4,7 @@ import sys
 if __package__ is None:
     sys.path.append(os.path.realpath("../"))
     sys.path.append(os.path.realpath("../../"))
-from CMS_Deep_Learning.utils import delphes_to_pandas
+from CMS_Deep_Learning.preprocessing.delphes_parse import delphes_to_pandas
 print(os.path.realpath("/data/shared/Delphes/wjets_lepFilter_13TeV/wjets_lepFilter_13TeV_183.root"))
 df = delphes_to_pandas(os.path.realpath("/data/shared/Delphes/wjets_lepFilter_13TeV/wjets_lepFilter_13TeV_183.root"),fixedNum=100)
 print(df)
