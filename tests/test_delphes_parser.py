a1b88b5de3da568286898ff747c2e551e379e465
Danny Weitekamp <dannyweitekamp@gmail.com>
2017-01-07 16:05:56 -0800

    More sanity checks on parser

--- tests/test_delphes_parser.py
+++ tests/test_delphes_parser.py
@@ -1,12 +1,28 @@
 import os
 import sys
 import unittest
+import pandas as pd
 
 if __package__ is None:
     sys.path.append(os.path.realpath("../"))
     sys.path.append(os.path.realpath("../../"))
 import CMS_Deep_Learning
-from CMS_Deep_Learning.preprocessing.delphes_parser import delphes_to_pandas
+from CMS_Deep_Learning.preprocessing.delphes_parser import delphes_to_pandas, ISO_TYPES
+
+def checkOmission(t,particles, tracks):
+    for entry, part_df in particles:
+        track_df = tracks.get_group(entry)
+        for row in part_df.itterrows():
+            t.assertFalse((track_df == row).all(1).any())
+
+def checkIsoNonNegative(t,df):
+    t.assertFalse((df[[tup[0] for tup in ISO_TYPES]] < 0.0).any(1).any(0))
+
+
+def checkFillTrackInfo(t,df):
+    t.assertTrue((df[['X', 'Y', 'Z', 'Dxy']] != 0.0).any(1).all(0))
+
+
 
 class TestDelphesParser(unittest.TestCase):
     def test_sanity(self):
@@ -15,10 +31,24 @@ class TestDelphesParser(unittest.TestCase):
         loc = os.path.abspath(loc)
         print(loc)
         self.assertTrue(os.path.exists(loc))
-        df = delphes_to_pandas(loc, fixedNum=10)
+        frames = delphes_to_pandas(loc, fixedNum=10)
+        self.assertTrue(isinstance(frames, dict))
+        self.assertFalse( False in [isinstance(val, pd.DataFrame) for key, val in frames.items()])
+        should_be = set(['Electron', 'MuonTight', 'MissingET', 'EFlowPhoton', 'EFlowNeutralHadron', 'EFlowTrack'])
+        self.assertFalse(should_be.issubset(set([key for key, val in frames.items()])))
+
+        electrons = frames["Electron"].groupby(["Entry"], group_keys=True)
+        muons = frames["MuonTight"].groupby(["Entry"], group_keys=True)
+        tracks = frames["EFlowTrack"].groupby(["Entry"], group_keys=True)
+        checkOmission(self, electrons, tracks)
+        checkOmission(self, muons, tracks)
+        checkFillTrackInfo(self, electrons)
+        checkFillTrackInfo(self, muons)
+        for df in frames.values():
+            checkIsoNonNegative(self, df)
+
         print(df)
 
 if __name__ == '__main__':
     unittest.main()
 
-print(df)
