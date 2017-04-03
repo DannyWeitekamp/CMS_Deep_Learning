9aabbb0fce2f7fe21912d9a4914f35fee8fa993d
Danny Weitekamp <dannyweitekamp@gmail.com>
2017-04-03 13:30:56 -0700

    update scripts

--- scripts/runDP.sh
+++ scripts/runDP.sh
@@ -10,8 +10,8 @@ export CRAY_CUDA_MPS=1
 
 export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
 module load daint-gpu
-module load h5py/2.6.0-CrayGNU-2016.11-Python-3.5.2-serial
-module load pycuda/2016.1.2-CrayGNU-2016.11-Python-3.5.2-cuda-8.0.54
+module load h5py/2.6.0-CrayGNU-2016.11-Python-2.7.12-serial
+module load pycuda/2016.1.2-CrayGNU-2016.11-Python-2.7.12-cuda-8.0.5
 
 source /scratch/snx3000/dweiteka/p2.7/bin/activate
 
