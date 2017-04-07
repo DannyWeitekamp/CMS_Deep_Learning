bc3dcddd8a9b3376c242bfdacff62f1352f03c16
Danny Weitekamp <dannyweitekamp@gmail.com>
2017-04-06 21:48:41 -0700

    to srun

--- scripts/runTrial.sh
+++ scripts/runTrial.sh
@@ -9,12 +9,12 @@
 export CRAY_CUDA_MPS=1
 
 export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
-module load daint-gpu
-module load h5py/2.6.0-CrayGNU-2016.11-Python-2.7.12-serial
-module load pycuda/2016.1.2-CrayGNU-2016.11-Python-2.7.12-cuda-8.0.54
+#module load daint-gpu
+#module load h5py/2.6.0-CrayGNU-2016.11-Python-2.7.12-serial
+#module load pycuda/2016.1.2-CrayGNU-2016.11-Python-2.7.12-cuda-8.0.54
 
 
 source /scratch/snx3000/dweiteka/kerasP2/bin/activate
 
-python $1scripts/runTrial.py $1 $2 $3 $4 $5 $6
+srun python $1scripts/runTrial.py $1 $2 $3 $4 $5 $6
 
